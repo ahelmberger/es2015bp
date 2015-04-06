53934efb7f6502349e80e6be7c207d96b916b287
Andreas Helmberger <public@ahelmberger.de>
2015-04-07 01:07:57 +0200

    Fix source map generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,20 @@
 "use strict";
 
-var fs                 = require("fs");
-var url                = require("url");
 var path               = require("path");
 var gulp               = require("gulp");
-var less               = require("less");
 var jspm               = require("jspm");
 var rimraf             = require("rimraf");
 var runSequence        = require("run-sequence");
-var browserSync        = require("browser-sync");
-var CleanCssPlugin     = require("less-plugin-clean-css");
-var AutoPrefixPlugin   = require("less-plugin-autoprefix");
 var historyApiFallback = require("connect-history-api-fallback");
 var tools              = require("require-dir")("build", { camelcase: true });
 var $                  = require("gulp-load-plugins")();
 
 var supportedBrowsers  = ["last 3 versions", "last 3 BlackBerry versions", "last 3 Android versions"];
-var autoPrefix         = new AutoPrefixPlugin({ browsers: supportedBrowsers });
-var cleanCss           = new CleanCssPlugin({ advanced: true });
 var exitOnError        = true;
 
-gulp.task("clean", function (done) {
-  rimraf("dist", { maxBusyTries: 5 }, done);
+gulp.task("clean", function () {
+  rimraf.sync(".tmp", { maxBusyTries: 5 });
+  rimraf.sync("dist", { maxBusyTries: 5 });
 });
 
 gulp.task("lint", function () {
@@ -31,18 +24,33 @@ gulp.task("lint", function () {
     .pipe($.if(exitOnError, $.eslint.failAfterError()));
 });
 
-gulp.task("build:styles", function () {
-  return gulp.src("app/main.less")
-    .pipe($.sourcemaps.init())
-    .pipe($.less({ plugins: [cleanCss, autoPrefix] }))
-    .pipe($.sourcemaps.write(".", { includeContent: false, sourceRoot: "." }))
-    .pipe(gulp.dest("dist"));
+gulp.task("styles", function (done) {
+  tools.compileLess({
+    from: "app/main.less",
+    to: ".tmp/main.css",
+    base: "app",
+    csswring: { removeAllComments: true },
+    autoprefixer: { browsers: supportedBrowsers }
+  }).then(done, done);
+});
+
+gulp.task("build:styles", function (done) {
+  tools.compileLess({
+    from: "app/main.less",
+    to: "dist/main.css",
+    base: "app",
+    sourceRoot: "/sources/",
+    csswring: { removeAllComments: true },
+    autoprefixer: { browsers: supportedBrowsers }
+  }).then(done, done);
 });
 
 gulp.task("build:scripts", function (done) {
-  var bundleOptions = { minify: true, mangle: true, sourceMaps: true, lowResSourceMaps: false };
+  var bundleOptions = { minify: true, mangle: false, sourceMaps: true, lowResSourceMaps: true };
   jspm.setPackagePath(".");
-  jspm.bundleSFX("main", "dist/main.js", bundleOptions).then(done, done);
+  jspm.bundleSFX("main", "dist/main.js", bundleOptions)
+    .then(function () { tools.sanitizeSourceMap("dist/main.js.map", "app", "/sources/"); })
+    .then(done, done);
 });
 
 gulp.task("build:html", function () {
@@ -53,16 +61,15 @@ gulp.task("build:html", function () {
 });
 
 gulp.task("build", function (done) {
-  runSequence("clean", "lint", "test", ["build:styles", "build:scripts", "build:html"], "sanitize-sourcemaps", done);
+  runSequence("clean", "lint", "test", ["build:styles", "build:scripts", "build:html"], done);
 });
 
-gulp.task("sanitize-sourcemaps", function (done) {
+gulp.task("sanitize-sourcemap", function (done) {
   tools.sanitizeSourceMaps("dist/*.map", "app", "/sources/", done);
 });
 
-gulp.task("serve", function () {
-  var lessFallbackOptions = { root: "app", plugins: [cleanCss, autoPrefix], sourceMap: true };
-  tools.startBrowserSync("app", [cssToLessFallback(lessFallbackOptions), historyApiFallback]);
+gulp.task("serve", ["styles"], function () {
+  tools.startBrowserSync([".tmp", "app"], [historyApiFallback]);
 });
 
 gulp.task("serve:dist", function () {
@@ -83,70 +90,11 @@ gulp.task("lint-and-test", function (done) {
   runSequence("lint", "test", done);
 });
 
-gulp.task("reload-styles", function () {
-  browserSync.reload("main.css");
-});
-
 gulp.task("watch", ["serve"], function () {
   exitOnError = false;
   gulp.watch(["*.js", "build/*.js"], ["lint"]);
   gulp.watch(["app/**/*.js"], ["lint-and-test"]);
-  gulp.watch(["app/**/*.less"], ["reload-styles"]);
+  gulp.watch(["app/**/*.less"], ["styles"]);
 });
 
 gulp.task("default", ["watch"]);
-
-function cssToLessFallback(options) {
-  var root = path.resolve(options.root);
-  return function (req, res, next) {
-    var requestedPath = url.parse(req.url).pathname;
-    if (requestedPath.match(/\.css$/)) {
-      var cssFile = path.resolve(path.join(root, requestedPath));
-      var lessFile = path.resolve(path.join(root, requestedPath.replace(/\.css$/, '.less')));
-      if (!fs.existsSync(cssFile) && fs.existsSync(lessFile)) {
-        var content = fs.readFileSync(lessFile).toString();
-        var lessOptions = { filename: lessFile, plugins: options.plugins };
-        if (options.sourceMap) {
-          lessOptions.sourceMap = { sourceMapFileInline: true, sourceMapBasepath: path.dirname(lessFile) };
-        }
-        return less.render(content, lessOptions)
-          .then(function (output) {
-            res.setHeader('Content-Type', 'text/css');
-            res.end(output.css);
-            next();
-          })
-          .catch(function (error) {
-            res.setHeader('Content-Type', 'text/css');
-            res.end(createCssErrorMessage(error));
-            next();
-          });
-      }
-    }
-    next();
-  };
-}
-
-function createCssErrorMessage(error) {
-  var rules = {
-    'display'    : 'block',
-    'z-index'    : '1000',
-    'position'   : 'fixed',
-    'top'        : '0',
-    'left'       : '0',
-    'right'      : '0',
-    'font-size'  : '.9em',
-    'padding'    : '1.5em 1em 1.5em 4.5em',
-    'color'      : 'white',
-    'background' : 'linear-gradient(#DF4F5E, #CE3741)',
-    'border'     : '1px solid #C64F4B',
-    'box-shadow' : 'inset 0 1px 0 #EB8A93, 0 0 .3em rgba(0, 0, 0, .5)',
-    'white-space': 'pre',
-    'font-family': 'monospace',
-    'text-shadow': '0 1px #A82734',
-    'content'    : '"' + error.toString().replace(/"/g, '\\"') + '"'
-  };
-  var combinedRules = Object.keys(rules).map(function (key) {
-    return key + ':' + rules[key];
-  });
-  return 'html::before{' + combinedRules.join(';') + '}';
-}
